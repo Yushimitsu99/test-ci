89ebac1b4e0909e9bdaed201847af6314de9be00
Yushimitsu99 <bdm.yssac@hotmail.fr>
2025-07-02 11:20:08 +0200

    commit

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-      <H1>Coucou</H1>
+      <H1>Coucou coucou</H1>
     </div>
   </header>
 
